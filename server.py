124363f146b84283437cb9ccd728102bab149616
nicolebuffett <office@easternfrontier.ca>
2026-06-29 16:41:18 -0230

    Update server.py

--- server.py
+++ server.py
@@ -97,5 +97,8 @@ USER QUESTION:
         return str(e)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
